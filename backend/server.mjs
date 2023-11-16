535995ab0cd707d2782a53f57e7e819d8c39d04d
CAYK ANDRADE MAGNANI <1091392313005@fatec-srv.local>
2023-11-16 20:03:36 -0300

    iefiw

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,8 +1,8 @@
-import { Express } from "express";
-import { cors } from "cors";
+import express  from "express";
+import cors from "cors";
 
 const PORT = process.env.PORT || 5050;
-const app = Express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
